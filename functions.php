993050a64048e3987417d4ad528e60c83e80f410
Clickadelic <toby.hopp@gmail.com>
2023-11-14 20:41:32 +0100

    German translation added

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ if(!class_exists('Twenty_Twenty_Four_Child')){
         function __construct(){
             add_action('wp_before_admin_bar_render', array($this, 'remove_logo'), 21);
             add_action('wp_enqueue_scripts', array($this, 'load_theme_scripts'), 21);
+			add_action('after_setup_theme', array($this, 'theme_translation_setup'), 21);
         }
 
         function load_theme_scripts(){
@@ -19,6 +20,10 @@ if(!class_exists('Twenty_Twenty_Four_Child')){
             	wp_get_theme()->get('Version')
             );
         }
+
+		function theme_translation_setup(){
+			load_theme_textdomain('twentytwentyfour-child', get_template_directory() . '/languages');
+		}
     }
 }
 
