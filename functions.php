df96741cae0d3baf20879aa93158802b366cd568
Clickadelic <toby.hopp@gmail.com>
2023-11-14 20:59:03 +0100

    unused remove_logo function removed

--- functions.php
+++ functions.php
@@ -4,13 +4,11 @@ if(!class_exists('Twenty_Twenty_Four_Child')){
     class Twenty_Twenty_Four_Child {
 
         function __construct(){
-            add_action('wp_before_admin_bar_render', array($this, 'remove_logo'), 21);
             add_action('wp_enqueue_scripts', array($this, 'load_theme_scripts'), 21);
-			add_action('after_setup_theme', array($this, 'theme_translation_setup'), 21);
+			add_action('after_setup_theme', array($this, 'load_child_theme_translations'), 21);
         }
 
         function load_theme_scripts(){
-
             $parent_style = 'twentytwentyfour';
             wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
             
@@ -21,7 +19,7 @@ if(!class_exists('Twenty_Twenty_Four_Child')){
             );
         }
 
-		function theme_translation_setup(){
+		function load_child_theme_translations(){
 			load_theme_textdomain('twentytwentyfour-child', get_template_directory() . '/languages');
 		}
     }
